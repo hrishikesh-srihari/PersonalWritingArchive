ce6b83e6d20165bd284804bfd8fcfe340d046779
Hrishikesh Srihari <rishisrihari9@gmail.com>
2022-01-15 12:11:17 +0800

    full commit

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.scss'
+import { AppProps } from 'next/app'
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
