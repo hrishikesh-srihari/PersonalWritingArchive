505393919e5e93810c5430080816240fbad476f9
Hrishikesh Srihari <rishisrihari9@gmail.com>
2022-01-15 12:24:29 +0800

    Edited home page schema

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,32 @@
-import Head from 'next/head'
+import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
-import { FeaturedPosts } from '../sections';
-
 
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Rishi's Writing Archive</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <FeaturedPosts/>
-
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => ( <PostCard post={post.node} key={post.title}/> ))}
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
-        
         <div className="lg:col-span-4 col-span-1">
-            <div className="lg:sticky relatve top-8">
-              <PostWidget/>
-              <Categories/>
-            </div>
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
-
   return {
-    props: { posts }
-  }
+    props: { posts },
+  };
 }
