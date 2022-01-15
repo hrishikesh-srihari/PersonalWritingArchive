1c9c51fced34f96b4674e2459bdbbcd931fa0230
Hrishikesh Srihari <rishisrihari9@gmail.com>
2022-01-15 13:44:52 +0800

    removed adjacency posts and featured posts carousel

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
-import { AdjacentPosts } from '../../sections';
 
 const PostDetails = ({ post }) => {
   const router = useRouter();
@@ -19,7 +18,6 @@ const PostDetails = ({ post }) => {
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
-            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
             <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} />
           </div>
