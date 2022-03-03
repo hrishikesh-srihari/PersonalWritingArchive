d2597c4aaad5bf55f846bda835ffbc3a632b29c9
Hrishikesh Srihari <rishisrihari9@gmail.com>
2022-03-03 10:17:27 +0800

    changed title

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
                 <div className="md:float-left block">
                     <Link href="/">
                         <span className="cursor-pointer font-bold text-4xl text-black">
-                            Rishi's Writing Archive
+                            Rishi's Writing Collection
                         </span>
                     </Link>
                 </div>
